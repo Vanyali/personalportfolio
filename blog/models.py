c13284224e8fa0617aa766f47961acbf74bb8848
Vanyali <mouramachado64@gmail.com>
2018-08-09 12:37:17 +0100

    HomePage Created BlogApp Created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here: Title, Publication Date, BodyText, Image
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
